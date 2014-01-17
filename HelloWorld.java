5eed9159e031384ba55d1c80b53afc3b81a1769c
rteb <>
2014-01-18 01:28:39 +0600

    12

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,7 +14,7 @@ g.setColor(Color.white);
 g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
-drawCenteredString("льбульдинва", d.width, d.height, g);
+drawCenteredString("Ильбульдин", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
